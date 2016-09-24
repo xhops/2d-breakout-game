daf9b2b683f8034721769b98eca4cdb4d4d06261
Derek Lee <dleedev7@gmail.com>
2016-09-24 01:59:45 -0400

    creating and animating the ball

--- js/javascript.js
+++ js/javascript.js
@@ -1,16 +1,23 @@
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
 
-// red rectangle
-ctx.beginPath();
-ctx.rect(20, 40, 50, 50);
-ctx.fillStyle = "#FF0000";
-ctx.fill();
-ctx.closePath();
+var x = canvas.width/2;
+var y = canvas.height - 30;
 
-// green circle
-ctx.beginPath();
-ctx.arc(240, 160, 20, 0, Math.PI*2, false);
-ctx.fillStyle = "green";
-ctx.fill();
-ctx.closePath();
+// creating a value to increment x and y coordinates
+var dx = 2;
+var dy = -2;
+
+function draw(){
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+	ctx.beginPath();
+	ctx.arc(x, y, 10, 0, Math.PI * 2);
+	ctx.fillStyle = "#0095DD";
+	ctx.fill();
+	ctx.closePath();
+	x += dx;
+	y += dy;
+}
+
+// every 10 miliseconds draw on canvas
+setInterval(draw, 10);
