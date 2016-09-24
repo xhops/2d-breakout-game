e435edf042b871fe4c4cb82c5ddc7b67fc1b830f
Derek Lee <dleedev7@gmail.com>
2016-09-24 02:13:55 -0400

    adding comments and cleaning up code

--- js/javascript.js
+++ js/javascript.js
@@ -8,13 +8,19 @@ var y = canvas.height - 30;
 var dx = 2;
 var dy = -2;
 
-function draw(){
-	ctx.clearRect(0, 0, canvas.width, canvas.height);
+// function to draw ball on canvas
+function drawBall(){
 	ctx.beginPath();
 	ctx.arc(x, y, 10, 0, Math.PI * 2);
 	ctx.fillStyle = "#0095DD";
 	ctx.fill();
 	ctx.closePath();
+}
+
+// function to animate ball on canvas and clear previous drawing of ball
+function draw(){
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+	drawBall();
 	x += dx;
 	y += dy;
 }
