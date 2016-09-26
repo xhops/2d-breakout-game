45bdcd2a741e13d61deca1e268de233c7f20c1ee
Derek Lee <dleedev7@gmail.com>
2016-09-25 21:58:23 -0400

    setting collision detection for bricks

--- js/javascript.js
+++ js/javascript.js
@@ -30,15 +30,16 @@ var brickPadding = 10;
 var brickOffsetTop = 30;
 var brickOffsetLeft = 30;
 
-// iterate through a 2 dimensional array
+// iterate through a 2 dimensional array and initialize bricks
 var bricks = [];
 for(c = 0; c < brickColumnCount; c++){
 	bricks[c] = [];
 	for (r = 0; r <brickRowCount; r++){
 		bricks[c][r] = {
 			x : 0,
-			y : 0
-		}
+			y : 0,
+			status : 1
+		};
 	}
 }
 
@@ -64,18 +65,22 @@ function drawPaddle(){
 function drawBricks() {
     for(c=0; c<brickColumnCount; c++) {
         for(r=0; r<brickRowCount; r++) {
-            var brickX = (c*(brickWidth+brickPadding))+brickOffsetLeft;
-            var brickY = (r*(brickHeight+brickPadding))+brickOffsetTop;
-            bricks[c][r].x = brickX;
-            bricks[c][r].y = brickY;
-            ctx.beginPath();
-            ctx.rect(brickX, brickY, brickWidth, brickHeight);
-            ctx.fillStyle = "#0095DD";
-            ctx.fill();
-            ctx.closePath();
+        	if (bricks[c][r].status == 1){
+            	var brickX = (c*(brickWidth+brickPadding))+brickOffsetLeft;
+            	var brickY = (r*(brickHeight+brickPadding))+brickOffsetTop;
+            	bricks[c][r].x = brickX;
+            	bricks[c][r].y = brickY;
+            	ctx.beginPath();
+            	ctx.rect(brickX, brickY, brickWidth, brickHeight);
+            	ctx.fillStyle = "#0095DD";
+            	ctx.fill();
+            	ctx.closePath();
+            }
         }
     }
 }
+
+
 
 // function to animate ball on canvas and clear previous drawing of ball
 function draw(){
@@ -112,6 +117,8 @@ function draw(){
 	}
 	// initialize draw paddle function
 	drawPaddle();
+	// initialize collision detection
+	collisionDetection();
 }
 
 //  checking leftPressed  boolean and setting it to true if executed
@@ -129,6 +136,21 @@ function keyUpHandler(e){
 		rightPressed = false;
 	} else if (e.keyCode == 37){
 		leftPressed = false;
+	}
+}
+
+// function for brick collision detection and updating status
+function collisionDetection(){
+	for(c = 0; c < brickColumnCount; c++){
+		for(r = 0; r < brickRowCount; r++){
+			var b = bricks[c][r];
+			if(b.status == 1){
+				if(x > b.x && x < b.x + brickWidth && y > b.y && y < b.y + brickHeight){
+					dy = -dy;
+					b.status = 0;
+				}
+			}
+		}
 	}
 }
 
